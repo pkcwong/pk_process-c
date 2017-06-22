09eeb33b20631e9ca9a752453a48100e4874fc29
Christopher Wong <christopherpk1996@gmail.com>
2017-06-22 19:32:40 +0800

    typedef

--- pk_process/pk_process.h
+++ pk_process/pk_process.h
@@ -11,7 +11,7 @@ typedef struct process_t Process;
 typedef struct queue_t Queue;
 
 extern int (*process_tick)(void);
-extern struct queue_t Service;
+extern Queue Service;
 extern void service_config(Queue* queue);
 extern void batch_config(Queue* queue);
 
@@ -20,8 +20,8 @@ struct process_t
 	bool kill_flag;
 	int first_tick;
 	int last_tick;
-	void (*onExecute)(struct process_t* this);
-	void (*onDestroy)(struct process_t* this);
+	void (*onExecute)(Process* this);
+	void (*onDestroy)(Process* this);
 	Queue* parent;
 	Process** reference;
 };
@@ -29,15 +29,15 @@ struct process_t
 struct queue_t
 {
 	int size;
-	struct process_t process[MAX_QUEUE_SIZE];
-	struct process_t* reference[MAX_QUEUE_SIZE];
-	void (*run_config)(struct queue_t* queue);
+	Process process[MAX_QUEUE_SIZE];
+	Process* reference[MAX_QUEUE_SIZE];
+	void (*run_config)(Queue* queue);
 };
 
 void process_clock(int (*ticks)(void));
 void process_init(Queue* queue, void (*run_config)(Queue* queue));
 
-Process** process_add(struct queue_t* queue, void (*onExecute)(Process* this), void (*onDestroy)(Process* this));
+Process** process_add(Queue* queue, void (*onExecute)(Process* this), void (*onDestroy)(Process* this));
 void set_kill_flag(Process* this);
 void process_kill(Process* this);
 
